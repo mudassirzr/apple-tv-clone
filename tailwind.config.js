4be41d37517d2d32a3627ebaf371a63562dd4c1e
Mudasir Zahoor <mudassirzahoor14@gmail.com>
2024-09-07 21:45:53 +0530

    added headers, images

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,28 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "rgb(0, 113, 227)",
       },
+      spacing: {
+        "3.5": "0.85rem",
+      },
+      backgroundImage: {
+        'hero-pattern': "url('./assets/img/hero-img.png')",
+      }
     },
   },
   plugins: [],
